1cca9345cd8b5bf1ad48082cf57f19aee7f917eb
hamdani <hamdani.lmekki@gmail.com>
2025-12-01 14:56:49 +0100

    ajout de shadcnUI/Tailwindcss/vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 
 import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
 
 // https://astro.build/config
 export default defineConfig({
+	  vite: {
+    plugins: [tailwindcss()],
+  },
 	// Enable React to support React JSX components.
 	integrations: [react()],
 });
